7eec293cb6e97f2ec1a508050f00b32c5e257c5f
Wembley G. Leach, Jr <wembley.gl@gmail.com>
2017-03-04 21:32:00 -0500

    Add infinite scrolling, dynamic page title based on current subreddit, and ability to switch subreddits by clicking link in nav

--- assets/js/vanilla.js
+++ assets/js/vanilla.js
@@ -1,29 +1,40 @@
 /**
  * Created by wembleyleach on 3/4/17.
  */
+'use strict';
+
 ;var post = function (spec) {
     var self = {};
 
-    self.thumbnail = spec.thumbnail || 'http://placehold.it/80';
-    self.title = spec.title || 'Placehold.it really works!';
+    var imgTest = /.*\.jpg$/;
+    self.thumbnail = imgTest.test(spec.thumbnail) && spec.thumbnail || 'http://placehold.it/80';
+    self.title = spec.title || 'placehold.it really works!';
     self.author = spec.author || 'John Smith';
+    self.url = spec.url || '#';
 
     self.toString = function () {
         return 'Thumbnail: ' + self.thumbnail +
             'Title: ' + self.title +
-            'Author: ' + self.author;
+            'Author: ' + self.author +
+            'URL: ' + self.url;
     };
 
     return self;
 };
+
 var vanilla = (function (post) {
 
     var s,
         self = {
             settings: function () {
                 return {
-                    defaultSubreddit: 'apple',
-                    container: document.querySelector('#js-container')
+                    root: 'https://www.reddit.com/r/',
+                    currentSubreddit: 'Apple',
+                    container: document.querySelector('.post-container'),
+                    navItems: document.querySelectorAll('.nav__item'),
+                    loadMore: document.querySelector('#load-more'),
+                    isLoadingMore: false,
+                    subredditTitle: document.querySelector('.subreddit-title')
                 };
             },
 
@@ -32,34 +43,68 @@ var vanilla = (function (post) {
             },
 
             bindUiEvents: function () {
-                // s.container.addEventListener('click', this.writeToContainer);
+                Array.prototype.forEach.call(s.navItems, function (element) {
+                    element.querySelector('a').addEventListener('click', self.handleNavItemClick);
+                });
+
+                s.loadMore.addEventListener("click", self.handleLoadMoreClick);
+            },
+
+            handleNavItemClick: function (event) {
+                self.isLoadingMore = false;
+                s.container.innerHTML = '';
+                s.currentSubreddit = event.target.textContent;
+                var url = s.root + s.currentSubreddit + '.json';
+                self.loadSubreddit(url, self.success, self.error);
+            },
+
+            handleLoadMoreClick: function () {
+                s.isLoadingMore = true;
+                var url = s.root + s.currentSubreddit +
+                    '.json?limit=26&after=' +
+                    s.loadMore.getAttribute("data-after");
+                self.loadSubreddit(url, self.success, self.error);
             },
 
             init: function () {
                 s = self.settings();
                 self.bindUiEvents();
-                self.loadSubreddit(s.defaultSubreddit, self.success, self.error);
+                var url = s.root + s.currentSubreddit + '.json';
+                self.loadSubreddit(url, self.success, self.error);
             },
 
             success: function (resp) {
                 var index,
-                    response = JSON.parse(resp).data;
+                    response = JSON.parse(resp).data,
+                    length = response.children.length;
 
-                for (index = 0; index < response.children.length; index += 1) {
+                if (!s.isLoadingMore) {
+                    s.container.innerHTML = '';
+                }
+
+                for (index = 0; index < length; index += 1) {
                     var child = response.children[index].data;
                     var p = post({
                         thumbnail: child.thumbnail,
                         title: child.title,
-                        author: child.author
+                        author: child.author,
+                        url: child.url
                     });
 
                     var postElement = self.makePostElement(p);
                     self.appendToContainer(postElement);
+                    var hr = document.createElement('hr');
+                    self.appendToContainer(hr);
                 }
+
+                s.loadMore.setAttribute("data-after", response.after);
+                s.subredditTitle.innerHTML = '';
+                var subredditTitle = document.createTextNode('r/' + s.currentSubreddit);
+                s.subredditTitle.appendChild(subredditTitle);
 
             },
 
-            makePostElement: function(post) {
+            makePostElement: function (post) {
                 var postDiv = document.createElement('div');
                 postDiv.className = 'post';
 
@@ -71,20 +116,22 @@ var vanilla = (function (post) {
 
                 var postTitle = document.createElement('h3');
                 postTitle.className = 'post__title';
+
+                var postUrl = document.createElement('a');
+                postUrl.setAttribute('href', post.url);
+                postUrl.setAttribute('target', '_blank');
                 var postTitleText = document.createTextNode(post.title);
-                postTitle.appendChild(postTitleText);
+                postUrl.appendChild(postTitleText);
+
+                postTitle.appendChild(postUrl);
                 postDiv.appendChild(postTitle);
 
                 var postAuthor = document.createElement('p');
                 postAuthor.className = 'post__author';
-                var postAuthorText = document.createTextNode('By: '  + post.author);
+                var postAuthorText = document.createTextNode('By: ' + post.author);
                 postAuthor.appendChild(postAuthorText);
                 postDiv.appendChild(postAuthor);
 
-                var hr = document.createElement('hr');
-                postDiv.appendChild(hr);
-
-                console.log(postDiv);
                 return postDiv;
             },
 
@@ -95,21 +142,19 @@ var vanilla = (function (post) {
                 };
             },
 
-            loadSubreddit: function (subreddit, onload, onerror) {
+            loadSubreddit: function (url, onload, onerror) {
                 var request = new XMLHttpRequest();
-                var url = 'https://www.reddit.com/r/' + subreddit + '.json';
                 request.open('GET', url);
 
                 request.onload = function () {
                     if (request.status >= 200 && request.status < 400) {
                         // Success!
-                        var resp = request.responseText;
-                        onload(resp);
+                        onload(request.responseText);
                     } else {
                         // We reached our target server, but it returned an error
                         throw {
                             name: 'LoadSubredditError',
-                            message: 'Failed to retrieve defaultSubreddit'
+                            message: 'Failed to retrieve currentSubreddit'
                         };
                     }
                 };
